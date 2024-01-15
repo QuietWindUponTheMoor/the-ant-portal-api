dd39ed00210f56b0dd630bdc2a4617247193947a
QuietWind01 <calebhesse42@gmail.com>
2024-01-14 19:31:23 -0600

    Added post views incrementation

--- lib/routes/post_data.ts
+++ lib/routes/post_data.ts
@@ -47,6 +47,15 @@ export class PostData {
                 }
             }
 
+            // If everything else to this point is good, increment view count
+            const increment_check: any = await this.incrementViewCount(post_id);
+            const increment_successful: boolean = increment_check.validated;
+            const increment_fail_reason: string = increment_check.reason;
+            if (!increment_successful) {
+                pass_fail = false;
+                fail_reason = increment_fail_reason;
+            }
+
             // Process data and send response
             let reply_data: any;
             if (pass_fail === false || post_data === null) {
@@ -81,6 +90,49 @@ export class PostData {
         });
     }
 
+    public async incrementViewCount(post_id: string): Promise<{validated: boolean, reason: string, post_data: any}> {
+        let fail_reason: string = "";
+        let pass_fail: boolean = true;
+        let post_data: any;
+        let view_count: number = 0;
+        let query_response: any;
+        
+        // Fetch current view count
+        const data: any = await db.query(`SELECT views FROM posts WHERE postID=?;`, [post_id]);
+        if (data[0].length < 1) {
+            pass_fail = false;
+            fail_reason = `Cannot fetch view_count for post ${post_id}.`;
+            post_data = null;
+        } else {
+            post_data = data[0][0];
+            view_count = post_data.views;
+        }
+
+        // If fetching current view count didn't fail, increment:
+        if (pass_fail !== false) { // If it DID fail by now
+            // Increment view count and update
+            view_count++;
+            query_response = await db.query("UPDATE posts SET views=? WHERE postID=?;", [view_count, post_id]);
+        }
+
+        // Get response data
+        const increment_response_data = query_response[0];
+        if (increment_response_data.serverStatus !== 2) {
+            // If request failed in any way
+            fail_reason = `Query failed: ${{server_status: data.serverStatus, details: data.info, warning_status: data.warningStatus}}`;
+            pass_fail = false;
+        } else {
+            console.log(`\x1b[33m Incremented view_count for post \x1b[32m${post_id}\x1b[0m`);
+        }
+
+        // If all else is good, return result
+        return {
+            validated: pass_fail,
+            reason: fail_reason,
+            post_data: post_data
+        }
+    }
+
     public async fetchPostData(post_id: string): Promise<{validated: boolean, reason: string, post_data: any}> {
         let fail_reason: string = "";
         let pass_fail: boolean = true;
@@ -90,7 +142,7 @@ export class PostData {
         const data: any = await db.query(`SELECT * FROM posts WHERE postID=?;`, [post_id]);
         if (data[0].length < 1) {
             pass_fail = false;
-            fail_reason = "No records were found for that user ID.";
+            fail_reason = `No records were found for the post ID ${post_id}.`;
             post_data = null;
         } else {
             post_data = data[0][0];
